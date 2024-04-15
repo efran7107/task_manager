2b7499fa3c05e239f13ea4f4e32a46838ca8d998
efran7107 <efran7107@gmail.com>
2024-04-15 09:51:03 -0600

    add db and api
    added db and api

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tasm Manager
+# Task Manager
 
 A task manager app that allows teams and individuals to keep track of what tasks to complete and achive their goals in a kanban style format
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,54 +1,54 @@
 export type Team = {
-	teamId: number;
-	teamName: string;
-	teamLeaderId: number;
+  teamId: number;
+  teamName: string;
+  teamLeaderId: number;
 };
 
 export type TeamMember = {
-	teamMemberId: number;
-	name: string;
-	email: string;
+  teamMemberId: number;
+  name: string;
+  email: string;
 };
 
 export type TeamMemberAuth = {
-	teamMemberId: number;
-	password: string;
+  teamMemberId: number;
+  password: string;
 };
 
 export type TeamMemberTeamsLink = {
-	TeamMemberId: number;
-	TeamId: number;
+  TeamMemberId: number;
+  TeamId: number;
 };
 
-export type Status = 'to-do' | 'doing' | 'done';
+export type Status = "to-do" | "doing" | "done";
 
 export type Task = {
-	taskId: number;
-	taskName: string;
-	description: string;
-	status: Status;
-	dueDate: Date;
-	isImportant: boolean;
+  taskId: number;
+  taskName: string;
+  description: string;
+  status: Status;
+  dueDate: Date;
+  isImportant: boolean;
 };
 
 export type TaskAssinmentLink = {
-	taskAssignmentID: number;
-	taskId: number;
+  teamMemberId: number;
+  taskId: number;
 };
 
 export type Tag = {
-	tagId: number;
-	TagName: string;
+  tagId: number;
+  TagName: string;
 };
 
 export type TaskTagLink = {
-	taskID: number;
-	tagId: number;
+  taskID: number;
+  tagId: number;
 };
 
 export type Notes = {
-	noteId: number;
-	content: string;
-	TeamMemberId: number;
-	TaskId: number;
+  noteId: number;
+  content: string;
+  TeamMemberId: number;
+  TaskId: number;
 };
