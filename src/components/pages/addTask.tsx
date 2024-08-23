412bc4cc6bd36be1b8b5de9880bfca7e6a66371b
efran7107 <efran7107@gmail.com>
2024-08-22 20:07:10 -0600

    need to add team profiles

--- src/components/pages/addTask.tsx
+++ src/components/pages/addTask.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { UserInput } from "../inputs/formInputs";
+import { defaultNewTask } from "../../functions/defaultStates";
+import { useUser } from "../../functions/providersContext";
 
 export const AddTask = () => {
+  const { user } = useUser();
+  const [newTask, setNewTask] = useState({ ...defaultNewTask, ucId: user.id });
+  const { title, desc, status, dueDate, dateCreated, isUrgent } = newTask;
   return (
     <div className="add-task-cont">
       <h2 className="add-task">Add Task</h2>
