dda8fad46979d898297f82f30e4b1c53d50fd96d
efran7107 <efran7107@gmail.com>
2025-02-15 14:09:29 -0700

    finished styling
    monving to note creation

--- src/components/createTask.tsx
+++ src/components/createTask.tsx
@@ -6,6 +6,7 @@ import {defTeam, defUser} from "../functions/default.ts";
 import {getUserData} from "../functions/apiFunctions.ts";
 import {getDate} from "../functions/functions.ts";
 import {UserInput} from "./inputs/userInput.tsx";
+import '../css/createTask.css';
 
 type creatTask = Omit<TTask, 'id' | 'teamId'>
 
@@ -13,7 +14,8 @@ type createTaskStates = {
 	createTask: creatTask,
 	activeUser: User,
 	userTeams: Team[],
-	activeTeam: Team
+	activeTeam: Team,
+	assignedUsers: number[]
 }
 
 
@@ -23,8 +25,8 @@ const defCreateTask: creatTask = {
 	isUrgent: false,
 	author: "",
 	status: "to-do",
-	creationDate: "0000-01-01",
-	dueDate: "0000-01-01"
+	creationDate: "1000-01-01",
+	dueDate: "1000-01-01"
 }
 export class CreateTask extends Component<{
 	setPage: (page: TPage) => void
@@ -35,7 +37,8 @@ export class CreateTask extends Component<{
 		createTask: defCreateTask,
 		activeUser: defUser,
 		userTeams: [defTeam],
-		activeTeam: defTeam
+		activeTeam: defTeam,
+		assignedUsers: [defUser.getId()]
 	}
 
 	componentDidMount() {
@@ -54,7 +57,8 @@ export class CreateTask extends Component<{
 					},
 					activeUser: user,
 					userTeams: userTeams,
-					activeTeam: activeTeam
+					activeTeam: activeTeam,
+					assignedUsers: [user.getId()]
 				})
 				this.props.setIsLoading(false)
 			})
@@ -63,28 +67,56 @@ export class CreateTask extends Component<{
 	setTask = (key: string, val: string | boolean | taskStatus) => {
 		this.setState({...this.state, createTask: {...this.state.createTask, [key]: val }})
 	}
+	
+	changeTeam = (teamName: string) => {
+		this.setState({
+			...this.state,
+			activeTeam: this.state.userTeams.find(team => team.getName() === teamName)!,
+			assignedUsers: [this.state.activeUser.getId()]
+		})
+	}
+	
+	addUser = (userId: number) => {
+		this.setState({
+			...this.state,
+			assignedUsers: [...this.state.assignedUsers, userId]
+		})
+	}
+	
+	removeUser = (userId: number) => {
+		const idIndex = this.state.assignedUsers.indexOf(userId)
+		const newArr = [...this.state.assignedUsers.slice(0, idIndex), ...this.state.assignedUsers.slice(idIndex + 1)]
+		this.setState({...this.state, assignedUsers: newArr})
+	}
 
 	render() {
-		const {createTask, activeTeam, userTeams, activeUser} = this.state
+		const {createTask, activeTeam, userTeams, activeUser, assignedUsers} = this.state
 		const { isUrgent, creationDate, dueDate} = createTask
 
 
 		return (
-			<form action="">
+			<form className='task-form-entry' action="">
 				<div className="task-info">
+					<h2>New Task:</h2>
 					<UserInput id='title' curKey='title' label='Title' userInput={{
 						onChange: (e) => {
 							this.setTask('title', e.currentTarget.value)
 						}
 					}}/>
-					<UserInput id='desc' curKey='desc' label='Desc' userInput={{
-						onChange: (e) => {
-							this.setTask('desc', e.currentTarget.value)
-						},
-					}}/>
+					<div className="user-desc-cont">
+						<label htmlFor='desc'>Task Description: </label>
+						<textarea style={{resize: "none"}}/>
+					</div>
 					<div className="is-urgent">
 						<label htmlFor="isUrgent">Urgent</label>
-						<input type="checkbox" defaultChecked={isUrgent}/>
+						<input
+							className='is-urgent-box'
+							type="checkbox"
+							defaultChecked={isUrgent}
+							onClick={() => {
+								this.setTask('isUrgent', isUrgent ? false : true)
+							}}
+						/>
 					</div>
 					<div className="due-date">
 						<label htmlFor="dueDate">Due Date: </label>
@@ -98,28 +130,59 @@ export class CreateTask extends Component<{
 					</div>
 				</div>
 				<div className="choose-team-cont">
+					<h2>Assignments:</h2>
 					<div className="choose-team">
 						<select
 							name="activeTeam"
 							id="activeTeam"
 							value={activeTeam.getName()}
 							onChange={
-								(e) => {
-									this.setState({...this.state, activeTeam: userTeams.find(team => team.getName() === e.currentTarget.value)!})
-								}
-							}
+								(e) =>
+									this.changeTeam(e.currentTarget.value)
+						}
 						>
 							{userTeams.map(team => (<option value={team.getName()} key={team.getId()}>{team.getName()}</option>))}
 						</select>
 					</div>
 					<div className="choose-users">
 						<div className="assign-to">
-							{activeTeam.getUsers().map((user) => {
-									if(user.getId() !== activeTeam.getTeamLeader().getId() && user.getId() !== activeUser.getId())
-										return(<p key={user.getId()}>{user.getUserNames().name}</p>)
-							})}
+							<h4>Available: </h4>
+							<div className="names-cont">
+								{
+									activeTeam.getUsers().map(user => {
+										if(
+											user.getId() !== activeTeam.getTeamLeader().getId() &&
+											user.getId() !== activeUser.getId() &&
+											assignedUsers.filter(id => id === user.getId()).length < 1
+										)
+											return (
+												<p
+													key={user.getId()}
+													onClick={() => this.addUser(user.getId())}
+												>
+													{user.getUserNames().name}
+												</p>)
+									})
+								}
+							</div>
 						</div>
-						<div className="been-assigned"></div>
+						<div className="been-assigned">
+							<h4>Assigned:</h4>
+							<div className="names-cont">
+								{
+									this.state.assignedUsers
+										.map(id => this.state.activeTeam.getUsers().find(user => user.getId() === id)!)
+										.filter(user => user.getId() !== activeTeam.getTeamLeader().getId() && user.getId() !== activeUser.getId())
+										.map(user =>
+											<p
+												key={user.getId()}
+												onClick={() => this.removeUser(user.getId())}
+											>
+												{user.getUserNames().name}
+											</p>)
+								}
+							</div>
+						</div>
 					</div>
 				</div>
 			</form>
